d5e1f1be372dd4dbef7b67350fd39704dda24558
Denis Klimov <tyferly2003@gmail.com>
2025-02-27 13:28:10 +0400

    добавил MINIO и также изображения к асанам

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
