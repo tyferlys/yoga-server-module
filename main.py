0b1e446dea57504a2ea72fa86f37a22be355921d
Denis Klimov <tyferly2003@gmail.com>
2025-02-27 13:47:15 +0400

    фикс куки

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://213.108.251.51:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
